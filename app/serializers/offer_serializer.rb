646e600a5a8eb0e36dd5402f8ede24f531512ab0
Van Celdran <celdranvan@gmail.com>
2023-07-30 15:57:14 -0700

    try 1 on fixing deletion of user bug

--- app/serializers/offer_serializer.rb
+++ app/serializers/offer_serializer.rb
@@ -3,10 +3,10 @@ class OfferSerializer < ActiveModel::Serializer
   attributes :id, :price, :contact, :user_name, :user_image_url
 
   def user_name
-    object.user.name
+    object.user&.name
   end
 
   def user_image_url
-    object.user.image_url
+    object.user&.image_url
   end
 end
